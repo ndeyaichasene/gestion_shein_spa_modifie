44988dace10599578fa28eee8b539681a0c873dd
Ndeye Aissatou Sene <ndeyaichasene@gmail.com>
2026-06-15 17:11:09 +0000

    apres modification du server

--- src/config.js
+++ src/config.js
@@ -4,4 +4,4 @@ export const API_BASE =
     (window.location.hostname === 'localhost' ||
      window.location.hostname === '127.0.0.1')
         ? 'http://localhost:10000'
-        : 'https://ton-backend.onrender.com';
+        : 'https://gestion-shein-spa-modifie.onrender.com';
